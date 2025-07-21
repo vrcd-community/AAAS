b5b8b858466e5ab307c014d0e44af9434353d61b
Xuan25 <xuan@xuan25.com>
2025-07-21 14:17:47 +0100

    chore: Update regex for Python-based gitscript for better line-ending compatibility

--- .gitscripts/filter_usharp.py
+++ .gitscripts/filter_usharp.py
@@ -4,17 +4,17 @@ import sys
 # This script filters out U# serialized data from Unity YAML files.
 
 # pattern to match U# serialized data
-guid_pattern = r'\{fileID: .*(\n\s+type: [0-9]+)?\}'
+guid_pattern = r'\{fileID: .*(\r?\n\s+type: [0-9]+)?\}'
 pattern = re.compile(
     "".join([
-        fr"    - target: {guid_pattern}\n",
-        r"      propertyPath: (serializedProgramAsset|serializationData\..*)\n",
-        r"      value:.*\n",
-        fr"      objectReference: {guid_pattern}\n",
+        fr"    - target: {guid_pattern}\r?\n",
+        r"      propertyPath: (serializedProgramAsset|serializationData\..*)\r?\n",
+        r"      value:.*\r?\n",
+        fr"      objectReference: {guid_pattern}\r?\n",
         r"|",
-        fr"  serialized(Udon)?ProgramAsset: {guid_pattern}\n",
+        fr"  serialized(Udon)?ProgramAsset: {guid_pattern}\r?\n",
         r"|",
-        r"    SerializedFormat: [02]\n",
+        r"    SerializedFormat: [02]\r?\n",
     ]),
     flags=re.MULTILINE
 )
