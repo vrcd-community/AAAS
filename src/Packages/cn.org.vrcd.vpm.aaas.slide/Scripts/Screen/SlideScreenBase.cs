3d53003d766acab8f5cc3ded024b0a256d53cd3a
Misaka-L <lipww1234@foxmail.com>
2025-07-23 01:47:16 +0800

    feat: slide screen component

---
+++ src/Packages/cn.org.vrcd.vpm.aaas.slide/Scripts/Screen/SlideScreenBase.cs
@@ -0,0 +1,26 @@
+﻿using AAAS.Slide.Core;
+using UdonSharp;
+using UnityEngine;
+
+namespace AAAS.Slide.Screen {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public abstract class SlideScreenBase : UdonSharpBehaviour {
+        [SerializeField] protected SlideCore slideCore;
+
+        private void Start() {
+            if (!slideCore) {
+                enabled = false;
+                Debug.LogError("[SlideScreenBase] SlideCore is not assigned. Please assign a SlideCore in the inspector.", this);
+                return;
+            }
+
+            var playerTexture = slideCore._GetSlidePlayerTexture();
+
+            SetScreenTexture(playerTexture);
+        }
+
+        protected virtual void SetScreenTexture(Texture texture) {
+            Debug.LogWarning("[SlideScreenBase] SetScreenTexture is not implemented. Please override this method in a derived class.", this);
+        }
+    }
+}
