a32d7482e4fd3cca11270c2dae0313c79a65b64e
Misaka-L <lipww1234@foxmail.com>
2025-07-25 15:12:53 +0800

    feat: slide url input controller

--- src/Packages/cn.org.vrcd.vpm.aaas.slide/Scripts/Controller/SlideController.cs
+++ src/Packages/cn.org.vrcd.vpm.aaas.slide/Scripts/Controller/SlideController.cs
@@ -2,7 +2,6 @@
 using JetBrains.Annotations;
 using UdonSharp;
 using UnityEngine;
-using VRC.SDK3.Components;
 
 namespace AAAS.Slide.Controller {
     /// <summary>
@@ -11,18 +10,6 @@ namespace AAAS.Slide.Controller {
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class SlideController : UdonSharpBehaviour {
         [SerializeField] protected SlideCore slideCore;
-
-        [SerializeField] private VRCUrlInputField urlInputField;
-
-        [PublicAPI]
-        public void LoadUrl() {
-            if (!slideCore || !urlInputField)
-                return;
-
-            var url = urlInputField.GetUrl();
-
-            slideCore._LoadSlide(url);
-        }
 
         [PublicAPI]
         public void NextPage() {
