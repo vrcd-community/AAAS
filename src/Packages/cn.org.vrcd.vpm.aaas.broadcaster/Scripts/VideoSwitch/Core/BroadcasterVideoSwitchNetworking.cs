29422734fa3596a34bee1ac5ad7f09df9c5eac47
Misaka-L <lipww1234@foxmail.com>
2025-07-30 15:17:58 +0800

    feat: video switch feature of broadcaster package

---
+++ src/Packages/cn.org.vrcd.vpm.aaas.broadcaster/Scripts/VideoSwitch/Core/BroadcasterVideoSwitchNetworking.cs
@@ -0,0 +1,39 @@
+﻿using JetBrains.Annotations;
+using UdonSharp;
+using VRC.SDKBase;
+
+namespace AAAS.Broadcaster.VideoSwitch.Core {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class BroadcasterVideoSwitchNetworking : UdonSharpBehaviour {
+        [UdonSynced] [FieldChangeCallback(nameof(CurrentInputIndex))]
+        private int _currentInputIndex;
+
+        [PublicAPI]
+        internal int CurrentInputIndex {
+            get => _currentInputIndex;
+            private set {
+                _currentInputIndex = value;
+                _broadcasterSwitch.SendCustomEvent(nameof(BroadcasterVideoSwitch._OnVideoInputIndexChanged));
+            }
+        }
+
+        private BroadcasterVideoSwitch _broadcasterSwitch;
+
+        internal void SetInputIndex(int index) {
+            TakeOwnership();
+            _currentInputIndex = index;
+            RequestSerialization();
+        }
+
+        internal void Initialize(BroadcasterVideoSwitch broadcasterSwitch) {
+            _broadcasterSwitch = broadcasterSwitch;
+        }
+
+        private void TakeOwnership() {
+            if (Networking.GetOwner(gameObject).isLocal)
+                return;
+
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+    }
+}
---
+++ src/Packages/cn.org.vrcd.vpm.aaas.broadcaster/Scripts/VideoSwitch/Input/SimpleTextureVideoInput.cs
@@ -0,0 +1,13 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace AAAS.Broadcaster.VideoSwitch.Input {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public sealed class SimpleTextureVideoInput : BroadcasterVideoInputBase {
+        [SerializeField] private Texture videoTexture;
+
+        public override Texture GetVideoTexture() {
+            return videoTexture;
+        }
+    }
+}
