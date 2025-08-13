fb90926e069c0648a0260bf2bde296fbe81a904b
Misaka-L <lipww1234@foxmail.com>
2025-08-13 15:18:09 +0800

    feat: protv audio adapter

---
+++ src/Packages/cn.org.vrcd.vpm.aaas.broadcaster.adaptor.protv/Scripts/ProTvBroadcasterTvPlugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ArchiTech.ProTV;
+using UdonSharp;
+using UnityEngine;
+
+namespace AAAS.Broadcaster.AudioControl.Adaptor.ProTV {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ProTvBroadcasterTvPlugin : TVPlugin {
+        private int _lastVpManagerHashCode = -1;
+
+        private AudioSource[] _cacheAudioSources = new AudioSource[0];
+
+        internal float GetVolume() => OUT_VOLUME;
+        internal void SetVolume(float volume) => tv._ChangeVolume(volume);
+
+        internal bool IsValidProTvInstance() {
+            return tv && tv.isReady && GetAudioSources().Length > 0;
+        }
+
+        internal AudioSource[] GetAudioSources() {
+            if (tv.ActiveManager.GetHashCode() == _lastVpManagerHashCode)
+                return _cacheAudioSources;
+
+            return GetAudioSourcesCore();
+        }
+
+        internal bool IsOwnerOfPlayer() => tv.IsOwner;
+
+        private AudioSource[] GetAudioSourcesCore() {
+            var vpManager = tv.ActiveManager;
+
+            var spatialSpeakers = (AudioSource[])vpManager.GetProgramVariable("spatialSpeakers");
+            var stereoSpeakers = (AudioSource[])vpManager.GetProgramVariable("stereoSpeakers");
+
+            var audioSources = new AudioSource[spatialSpeakers.Length + stereoSpeakers.Length];
+            Array.Copy(spatialSpeakers, 0, audioSources, 0, spatialSpeakers.Length);
+            Array.Copy(stereoSpeakers, 0, audioSources, spatialSpeakers.Length, stereoSpeakers.Length);
+
+            _lastVpManagerHashCode = vpManager.GetHashCode();
+            _cacheAudioSources = audioSources;
+            return audioSources;
+        }
+    }
+}
---
+++ src/Packages/cn.org.vrcd.vpm.aaas.broadcaster/Scripts/Tools/LoudnessTools.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace AAAS.Broadcaster.Tools {
+    public static class LoudnessTools {
+        [PublicAPI]
+        public static float GetRmsLoudness(float[] audioData, float smoothing = 0.8f, float lastRms = 0f) {
+            float sum = 0f;
+            foreach (float sample in audioData)
+            {
+                sum += sample * sample;
+            }
+
+            var newRms = Mathf.Sqrt(sum / audioData.Length);
+
+            return Mathf.Lerp(lastRms, newRms, 1 - smoothing);
+        }
+
+        [PublicAPI]
+        public static int GetWindowSize(int windowMs = 50) {
+            return (int)(AudioSettings.outputSampleRate * windowMs / 1000f);
+        }
+    }
+}
