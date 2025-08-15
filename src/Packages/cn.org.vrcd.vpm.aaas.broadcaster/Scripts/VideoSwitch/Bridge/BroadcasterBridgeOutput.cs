fcbdb5e77fa301f8c23fa6e82b464b41975dcc7b
Misaka-L <lipww1234@foxmail.com>
2025-08-15 22:24:49 +0800

    feat: broadcaster bridge

---
+++ src/Packages/cn.org.vrcd.vpm.aaas.broadcaster/Scripts/VideoSwitch/Bridge/BroadcasterBridgeOutput.cs
@@ -0,0 +1,40 @@
+﻿using AAAS.Broadcaster.VideoSwitch.Output;
+using JetBrains.Annotations;
+using UdonSharp;
+using UnityEngine;
+
+namespace AAAS.Broadcaster.VideoSwitch.Bridge {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class BroadcasterBridgeOutput : BroadcasterVideoOutputBase {
+        private BroadcasterBridgeInput[] _bridgeInputs = new BroadcasterBridgeInput[0];
+
+        protected override void UpdateOutputTexture(Texture texture) {
+            foreach (var broadcasterBridgeInput in _bridgeInputs) {
+                if (!broadcasterBridgeInput) {
+                    Debug.LogError("[BroadcasterBridgeOutput] Found a null input in the bridge inputs array.");
+                    continue;
+                }
+
+                broadcasterBridgeInput.UpdateOutputTexture(texture);
+            }
+        }
+
+        internal void RegisterInput(BroadcasterBridgeInput input) {
+            if (!input) {
+                Debug.LogError("[BroadcasterBridgeOutput] Attempted to register a null input.");
+                return;
+            }
+
+            var newInputs = new BroadcasterBridgeInput[_bridgeInputs.Length + 1];
+            _bridgeInputs.CopyTo(newInputs, 0);
+            newInputs[_bridgeInputs.Length] = input;
+
+            _bridgeInputs = newInputs;
+        }
+
+        [CanBeNull]
+        internal Texture GetOutputTexture() {
+            return broadcasterSwitch._GetOutputTexture();
+        }
+    }
+}
