9cc0795a84abe71868b456027f3fad38dd4c0c5b
Misaka-L <lipww1234@foxmail.com>
2025-08-18 01:55:55 +0800

    feat: add Xuan25's Player Voice System as package

---
+++ src/Packages/com.xuan25.player-voice-system/Debug/Runtime/RoomDebuggingPanel.cs
@@ -0,0 +1,102 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Xuan25.PlayerVoiceSystem.Debugging
+{
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class RoomDebuggingPanel : UdonSharpBehaviour
+    {
+        public PlayerVoiceRoomController playerVoiceRoomController;
+
+        public GameObject rowPrefab;
+
+        public GameObject rowContainer;
+
+        private RoomDebuggingRow[] rows;
+
+        private int roomValidityMask;
+
+        int playerCountMax;
+
+        void Start()
+        {
+            if (playerVoiceRoomController == null)
+            {
+                Debug.LogError($"[{GetUdonTypeName()}] PlayerVoiceRoomController is not set up correctly.");
+                return;
+            }
+
+            int roomCountMax = playerVoiceRoomController.playerVoiceRooms.Length;
+
+            roomValidityMask = 0;
+            for (int i = 0; i < roomCountMax; i++)
+            {
+                if (playerVoiceRoomController.playerVoiceRooms[i] != null)
+                {
+                    roomValidityMask |= 1 << i;
+                }
+            }
+
+            playerCountMax = playerVoiceRoomController.playerVoiceRoomMask.Length;
+            rows = new RoomDebuggingRow[playerCountMax];
+            for (int i = 0; i < playerCountMax; i++)
+            {
+                GameObject rowObject = Instantiate(rowPrefab, rowContainer.transform);
+                rowObject.SetActive(false); // Initially hide the row
+                RoomDebuggingRow row = rowObject.GetComponent<RoomDebuggingRow>();
+                row.Setup(roomCountMax);
+                rows[i] = row;
+            }
+
+            OnPlayerListChanged();
+        }
+
+        public void OnPlayerListChanged()
+        {
+            // Debug.Log($"[{GetUdonTypeName()}] OnPlayerListChanged called. Updating player rows...");
+            SendCustomEventDelayedFrames(nameof(UpdatePlayerList), 1);
+        }
+
+        public void UpdatePlayerList()
+        {
+            for (int i = 0; i < playerCountMax; i++)
+            {
+                VRCPlayerApi player = VRCPlayerApi.GetPlayerById(i);
+                if (player == null)
+                {
+                    rows[i].gameObject.SetActive(false);
+                    continue;
+                }
+
+                rows[i].gameObject.SetActive(true);
+                rows[i].SetUserName(player.displayName);
+            }
+
+            UpdatePlayerState();
+        }
+
+        public void OnPlayerStateChanged()
+        {
+            // Debug.Log($"[{GetUdonTypeName()}] OnPlayerStateChanged called. Updating player masks...");
+            UpdatePlayerState();
+        }
+        
+        public void UpdatePlayerState()
+        {
+            for (int i = 0; i < playerCountMax; i++)
+            {
+                VRCPlayerApi player = VRCPlayerApi.GetPlayerById(i);
+                if (player == null) continue;
+
+                rows[i].SetMask(playerVoiceRoomController.playerVoiceRoomMask[i], roomValidityMask);
+                // Debug.Log($"[{GetUdonTypeName()}] Updated mask for player {player.displayName} (ID: {i}) with mask: {playerVoiceRoomController.playerVoiceRoomMask[i]} and room validity mask: {roomValidityMask}");
+            }
+        }
+
+    }
+
+}
---
+++ src/Packages/com.xuan25.player-voice-system/Runtime/PlayerVoiceRoom.cs
@@ -0,0 +1,168 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Xuan25.PlayerVoiceSystem
+{
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class PlayerVoiceRoom : UdonSharpBehaviour
+    {
+
+        #region Player Cache Management
+
+        private VRCPlayerApi[] playersCache;
+
+        private bool playersCacheInvalid = true;
+
+        private VRCPlayerApi[] GetPlayerList()
+        {
+            if (playersCacheInvalid || playersCache == null || playersCache.Length != VRCPlayerApi.GetPlayerCount())
+            {
+                playersCache = new VRCPlayerApi[VRCPlayerApi.GetPlayerCount()];
+                VRCPlayerApi.GetPlayers(playersCache);
+                playersCacheInvalid = false;
+            }
+            return playersCache;
+        }
+
+        private void InvalidatePlayerCache()
+        {
+            playersCacheInvalid = true;
+        }
+
+        #endregion
+
+        #region Room Management
+
+        private int roomID;
+
+        private PlayerVoiceRoomController playerVoiceRoomController;
+
+        [SerializeField]
+        private bool activeUpdate = false;
+
+        private bool[] playerMask;
+
+        private bool[] playerMaskDirty;
+
+        public void Setup(PlayerVoiceRoomController controller, int roomID)
+        {
+            this.playerVoiceRoomController = controller;
+            this.roomID = roomID;
+
+            playerMask = new bool[controller.playerVoiceRoomMask.Length];
+            for (int i = 0; i < playerMask.Length; i++)
+            {
+                playerMask[i] = false;
+            }
+            playerMaskDirty = new bool[controller.playerVoiceRoomMask.Length];
+            Array.Copy(playerMask, playerMaskDirty, playerMask.Length);
+        }
+
+        void Start()
+        {
+
+        }
+
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (activeUpdate) return;
+            if (!Utilities.IsValid(player)) return;
+
+            if (playerVoiceRoomController == null)
+            {
+                Debug.LogError("[PlayerVoiceRoom] PlayerVoiceRoomController is not set up correctly.");
+                return;
+            }
+
+            playerVoiceRoomController.OnPlayerRoomEnter(player, roomID);
+
+        }
+
+        public override void OnPlayerTriggerExit(VRCPlayerApi player)
+        {
+            if (activeUpdate) return;
+            if (!Utilities.IsValid(player)) return;
+
+            if (playerVoiceRoomController == null)
+            {
+                Debug.LogError("[PlayerVoiceRoom] PlayerVoiceRoomController is not set up correctly.");
+                return;
+            }
+
+            playerVoiceRoomController.OnPlayerRoomLeave(player, roomID);
+        }
+
+        public override void OnPlayerTriggerStay(VRCPlayerApi player)
+        {
+            if (!activeUpdate) return;
+            if (!Utilities.IsValid(player)) return;
+
+            if (playerVoiceRoomController == null)
+            {
+                Debug.LogError("[PlayerVoiceRoom] PlayerVoiceRoomController is not set up correctly.");
+                return;
+            }
+
+            playerMaskDirty[player.playerId] = true;
+        }
+
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            if (!activeUpdate) return;
+            if (!Utilities.IsValid(player)) return;
+
+            playerMask[player.playerId] = false;
+            playerMaskDirty[player.playerId] = false;
+
+            InvalidatePlayerCache();
+        }
+
+        public override void OnPlayerLeft(VRCPlayerApi player)
+        {
+            if (!activeUpdate) return;
+            if (!Utilities.IsValid(player)) return;
+
+            playerMask[player.playerId] = false;
+            playerMaskDirty[player.playerId] = false;
+
+            InvalidatePlayerCache();
+        }
+
+        void FixedUpdate()
+        {
+            if (!activeUpdate) return;
+
+            VRCPlayerApi[] vRCPlayerApis = GetPlayerList();
+            for (int i = 0; i < vRCPlayerApis.Length; i++)
+            {
+                VRCPlayerApi player = vRCPlayerApis[i];
+                if (!Utilities.IsValid(player))
+                    continue;
+                int playerId = player.playerId;
+                if (playerMaskDirty[playerId] != playerMask[playerId])
+                {
+                    if (playerMaskDirty[playerId])
+                    {
+                        playerMask[playerId] = true;
+                        playerVoiceRoomController.OnPlayerRoomEnter(player, roomID);
+                    }
+                    else
+                    {
+                        playerMask[playerId] = false;
+                        playerVoiceRoomController.OnPlayerRoomLeave(player, roomID);
+                    }
+                }
+                // Reset dirty state after processing, awaiting next FixedUpdate to set it again
+                playerMaskDirty[playerId] = false;
+            }
+        }
+
+        #endregion
+    }
+
+}
