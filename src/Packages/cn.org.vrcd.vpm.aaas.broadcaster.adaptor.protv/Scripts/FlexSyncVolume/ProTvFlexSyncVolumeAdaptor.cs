b7bbf1824092f0d2289e9b26726c1a0891f8442b
Misaka-L <lipww1234@foxmail.com>
2025-08-27 02:37:24 +0800

    feat: flex sync volume for pro tv

---
+++ src/Packages/cn.org.vrcd.vpm.aaas.broadcaster.adaptor.protv/Scripts/FlexSyncVolume/ProTvFlexSyncVolumeAdaptor.cs
@@ -0,0 +1,57 @@
+﻿using AAAS.Broadcaster.AudioControl.Source;
+using AAAS.Broadcaster.Tools;
+using AAAS.ProTvPlugins;
+using UdonSharp;
+
+namespace AAAS.Broadcaster.AudioControl.Adaptor.ProTV.FlexSyncVolume {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ProTvFlexSyncVolumeAdaptor : BroadcasterAudioSourceAdaptorBase {
+        public ProTvBroadcasterTvPlugin tvPlugin;
+        public ProTvFlexSyncVolume flexSyncVolumePlugin;
+
+        private float[] _audioData = new float[0];
+        private float _lastRmsLoudness;
+
+        public bool isLocalVolumeMode;
+
+        public override bool IsVolumeGetterSupported() => IsValidProTvInstance();
+        public override bool IsVolumeSetterSupported() => IsValidProTvInstance();
+
+        public override bool IsRmsLoudnessSupported() => IsValidProTvInstance();
+
+        public override void SetVolume(float volume) {
+            if (isLocalVolumeMode) {
+                flexSyncVolumePlugin._SetLocalVolume(volume);
+                return;
+            }
+
+            flexSyncVolumePlugin._SetSyncVolume(volume);
+        }
+
+        public override float GetVolume() => isLocalVolumeMode
+            ? flexSyncVolumePlugin._GetLocalVolume()
+            : flexSyncVolumePlugin._GetSyncVolume();
+
+        public override float GetRmsLoudness() {
+            if (!IsValidProTvInstance())
+                return 0f;
+
+            if (_audioData.Length == 0) {
+                var windowSize = LoudnessTools.GetWindowSize();
+                _audioData = new float[windowSize];
+            }
+
+            var audioSources = tvPlugin.GetAudioSources();
+            var tempSource = audioSources[0];
+
+            tempSource.GetOutputData(_audioData, 0);
+
+            _lastRmsLoudness = LoudnessTools.GetRmsLoudness(_audioData, lastRms: _lastRmsLoudness);
+            return _lastRmsLoudness;
+        }
+
+        private bool IsValidProTvInstance() {
+            return tvPlugin && tvPlugin.IsValidProTvInstance();
+        }
+    }
+}
---
+++ src/Packages/cn.org.vrcd.vpm.aaas.pro-tv-plugins/Scripts/ProTvFlexSyncVolume.cs
@@ -0,0 +1,54 @@
+﻿using ArchiTech.ProTV;
+using JetBrains.Annotations;
+using UdonSharp;
+
+namespace AAAS.ProTvPlugins {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class ProTvFlexSyncVolume : TVPlugin {
+        [UdonSynced]
+        private float _syncVolume = 1f;
+        
+        private float _localVolumeScale = 1f;
+        
+        [PublicAPI]
+        public float _GetSyncVolume() => _syncVolume;
+        [PublicAPI]
+        public float _GetLocalVolume() => _localVolumeScale;
+
+        [PublicAPI]
+        public bool _SetSyncVolume(float volume) {
+            if (volume < 0 || volume > 1f) {
+                Error("Sync Volume must be between 0 and 1");
+                return false;
+            }
+
+            Owner = localPlayer;
+            _syncVolume = volume;
+            RequestSerialization();
+            
+            UpdateVolumeCore();
+            return true;
+        }
+
+        [PublicAPI]
+        public bool _SetLocalVolume(float volume) {
+            if (volume < 0 || volume > 1f) {
+                Error("Local Volume Scale must be between 0 and 1");
+                return false;
+            }
+            
+            _localVolumeScale = volume;
+            UpdateVolumeCore();
+            return true;
+        }
+
+        private void UpdateVolumeCore() {
+            var targetVolume = _syncVolume * _localVolumeScale;
+            tv._ChangeVolume(targetVolume);
+        }
+
+        public override void OnDeserialization() {
+            UpdateVolumeCore();
+        }
+    }
+}
